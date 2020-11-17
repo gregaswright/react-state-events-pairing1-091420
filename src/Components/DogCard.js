c22a149a55a9e5ce5326ce6e3be4f68464eec839
bfeldman <brian.feldman42@gmail.com>
2020-11-17 10:43:22 -0500

    small tweaks

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -3,31 +3,31 @@ import React, { Component } from "react";
 class DogCard extends React.Component {
     constructor() {
       super()
-      this.state = {hasBarked: ""}
+      this.state = {hasBarked: null}
     }
     
     clickHandler = (event) => {
-      if (this.state.hasBarked === "") {
-        this.setState({hasBarked: "BARK"})
-      } else if (this.state.hasBarked === "BARK") {
-        this.setState({hasBarked: ""})
+      if (this.state.hasBarked === null) {
+        this.setState({hasBarked: `BARK`})
+      } else if (this.state.hasBarked !== null) {
+        this.setState({hasBarked: null})
       }
     }
   
     render() {
       return (
-      <div className="card" key={this.props.id}>
-        <span className="content">
-          <h2 >{this.props.name}</h2>
-          <img alt={this.props.name} src={this.props.img} />
-        </span>
-        <span className="bark">
-          <button onClick={this.clickHandler}>Bark</button>
-          <h2>{this.state.hasBarked}</h2>
-        </span>
-      </div>
-    )
+        <div className="card" key={this.props.id}>
+          <span className="content">
+            <h2 >{this.props.name}</h2>
+            <img alt={this.props.name} src={this.props.img} />
+          </span>
+          <span className="bark">
+            <button onClick={this.clickHandler}>Bark</button>
+            <h2>{this.state.hasBarked}</h2>
+          </span>
+        </div>
+      )
+    }
   }
-}
 
 export default DogCard;
