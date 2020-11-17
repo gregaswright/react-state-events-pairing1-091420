7260f09a09c6d8fba09d132b5e907a0cd6286385
bfeldman <brian.feldman42@gmail.com>
2020-11-17 10:21:53 -0500

    done

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-function DogCard() {
-  return (
-    <div className="card">
-      <span className="content">
-        <h2 >{/*Dog name goes here*/}</h2>
-        <img alt="" src={""} />
-      </span>
-      <span className="bark">
-        <button>Bark</button>
-      </span>
-    </div>
-  );
+class DogCard extends React.Component {
+    constructor() {
+      super()
+      this.state = {hasBarked: ""}
+    }
+    
+    clickHandler = (event) => {
+      if (this.state.hasBarked === "") {
+        this.setState({hasBarked: "BARK"})
+      } else if (this.state.hasBarked === "BARK") {
+        this.setState({hasBarked: ""})
+      }
+    }
+  
+    render() {
+      return (
+      <div className="card" key={this.props.id}>
+        <span className="content">
+          <h2 >{this.props.name}</h2>
+          <img alt={this.props.name} src={this.props.img} />
+        </span>
+        <span className="bark">
+          <button onClick={this.clickHandler}>Bark</button>
+          <h2>{this.state.hasBarked}</h2>
+        </span>
+      </div>
+    )
+  }
 }
 
 export default DogCard;
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import {apiResponse} from '../api.js'
+import DogCard from '../Components/DogCard.js'
+
 
 class DogsList extends Component {
+  dogCards = apiResponse.map(dog => <DogCard name={dog.name} img={dog.img} key={dog.id}/>)
   render() {
-    return <div className="list">{/*list of dogs go here*/}</div>;
+    console.log(this.dogCards)
+    return <div className="list">{this.dogCards}</div>;
   }
 }
 
